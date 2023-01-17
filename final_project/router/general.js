618bd1b130b432da8a9ec1f4560325fcac3df2e9
mmiller99 <mmille60@binghamton.edu>
2023-01-16 23:25:52 -0500

    started node project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,13 +13,17 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  return res.status(300).send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.body.isbn;
+  console.log(isbn);
+  console.log(req.body)
+  return res.status(300).send(books[isbn]);
  });
   
 // Get book details based on author
